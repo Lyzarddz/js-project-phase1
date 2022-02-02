015602f0d5586a4a40586894b7e7d2287fedd666
Alyssa Falcione <lyzarddz@gmail.com>
2022-02-02 18:34:13 -0500

    JS frame started

---
+++ js/index.js
@@ -0,0 +1,22 @@
+ /** Global Variables */
+
+
+/** Node Retrieval */
+const mainDiv = () => document.getElementById("main");
+
+
+
+ /*Event Listeners*/
+
+
+ /** Event Handlers */ 
+const 
+
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2)");
+const p = document.createElement("p");
+
+
+ /** Start Up */
+
+ document.addEventListener("DOMCOntentLoaded",)
