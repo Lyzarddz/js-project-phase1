7da4af30c24fde602c0685b8679505dab35b8b80
Alyssa Falcione <lyzarddz@gmail.com>
2022-02-11 00:10:26 -0500

    Added photos

--- js/index.js
+++ js/index.js
@@ -2,21 +2,44 @@
 
 
 /** Node Retrieval */
+
 const mainDiv = () => document.getElementById("main");
+const startBtn = () => document.getElementById("start-btn")
+const restartBtn = () => document.getElementById("restart-btn");
 
-
-
+ 
  /*Event Listeners*/
+
+ const startBtnClick = () => {
+      startBtn.addEventListener("click", )
+ } 
 
 
  /** Event Handlers */ 
-const h3 = document.createElement("h3")
 
-const h1 = document.createElement("h1");
-const h2 = document.createElement("h2)");
-const p = document.createElement("p");
+const onLoad  = () => {
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
 
+    console.log(p);
+
+    h1.className = "center-align";
+    p.className = "left-align";
+    
+
+    h1.innerText = "Matches: ";
+    p.innerText = "Instructions on how to play go here";
+
+    mainDiv().appendChild(h1);
+    mainDiv().appendChild(p);
+}
+
+onLoad();
 
  /** Start Up */
 
- document.addEventListener("DOMCOntentLoaded", functiongoeshere);
+ document.addEventListener("DOMCOntentLoaded", ()=> {
+     
+    
+ });
